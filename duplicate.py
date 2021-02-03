89a527b99a1f49bd6b5294b6d77f44c4fb703f4d
Taina Coleman <tainagdcoleman@gmail.com>
2021-02-02 16:57:54 -0800

    Adds command line arguments

--- duplicate.py
+++ duplicate.py
@@ -6,6 +6,7 @@ from typing import Set
 from uuid import uuid4
 from find_microstructures import draw
 import random
+import argparse
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
@@ -30,12 +31,21 @@ def duplicate_nodes(graph: nx.DiGraph, nodes: Set[str]):
                 graph.add_edge(new_node, child)
 
 
-def duplicate(microstructure: pathlib.Path, base_graph: pathlib.Path, nodes: int) -> nx.DiGraph:
+def duplicate(microstructure: pathlib.Path, base_graph: pathlib.Path, nodes: int, complex: bool = False) -> nx.DiGraph:
     microstructure = pathlib.Path(microstructure).resolve()
     base_graph = pathlib.Path(base_graph).resolve()
 
     mdata = json.loads(microstructure.read_text())
     graph: nx.DiGraph = pickle.loads(base_graph.read_bytes())
+
+    if not complex:
+        mdata = [ms for ms in mdata if ms["simple"]]
+    
+    if not mdata:
+        if not complex:
+            raise ValueError("Worflow has no simple microstructures")
+        else:
+            raise ValueError("Worflow has no microstructures")
 
     i = 0
     while len(graph.nodes) < nodes:
@@ -44,14 +54,38 @@ def duplicate(microstructure: pathlib.Path, base_graph: pathlib.Path, nodes: int
 
     return graph
 
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-w", "--workflow", 
+        choices=[path.stem for path in this_dir.joinpath("microstructures").glob("*") if path.is_dir()],
+        required=True,
+        help="Workflow to duplicate"
+    )
+    parser.add_argument(
+        "-s", "--size", type=int,
+        help="Approximate size of graph to generate"
+    )
+    parser.add_argument(
+        "-o", "--out", type=pathlib.Path,
+        help="path to save graph image to"
+    )
+    parser.add_argument(
+        "-c", "--complex", action="store_true",
+        help="Duplicate complex microstructures - this may not result in accurate looking graphs."
+    )
+
+    return parser
 
 def main():
+    parser = get_parser()
+    args = parser.parse_args()
 
-    path = this_dir.joinpath("microstructures", "1000genome")
-    graph = duplicate(path.joinpath("microstructures.json"), path.joinpath("base_graph.pickle"), 200)
+    path = this_dir.joinpath("microstructures", args.workflow)
+    graph = duplicate(path.joinpath("microstructures.json"), path.joinpath("base_graph.pickle"), args.size, args.complex)
     
     duplicated = {node for node in graph.nodes if "duplicate_of" in graph.nodes[node]}
-    draw(graph, save=this_dir.joinpath("duplicated.png"), close=True, subgraph=duplicated)
+    draw(graph, save=args.out, close=True, subgraph=duplicated)
 
 
 
