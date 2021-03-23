fc31d5647f7496b5318f054ed2da08adc3cd9887
Taina Coleman <tainagdcoleman@gmail.com>
2021-03-22 19:43:51 -0700

    Adds caching for quick recomputing (and --no-cache option)

--- wfchef/metric.py
+++ wfchef/metric.py
@@ -37,6 +37,7 @@ def get_parser()-> argparse.ArgumentParser:
         help="Workflow to duplicate"
     )
     parser.add_argument("-v", "--verbose", action="store_true", help="print logs")
+    parser.add_argument("--no-cache", action="store_true", help="if set, everything is recomputed from scratch")
 
     return parser
 
@@ -50,21 +51,23 @@ def main():
     verbose = args.verbose
 
     workflow = this_dir.joinpath("microstructures", args.workflow)
+    results_path = workflow.joinpath("results.json")
     summary = json.loads(workflow.joinpath("summary.json").read_text())
     sorted_graphs = sorted([name for name, _ in summary["base_graphs"].items()], key=lambda name: summary["base_graphs"][name]["order"])
+
+    results = {}
+    if not args.no_cache and results_path.is_file():
+        results = json.loads(results_path.read_text())
+
     labels = [f"{graph} ({summary['base_graphs'][graph]['order']})" for graph in sorted_graphs]
     labels = [summary['base_graphs'][graph]['order'] for graph in sorted_graphs]
     rows = [[None for _ in range(len(sorted_graphs))] for _ in range(len(sorted_graphs))]
-    results = {}
     
     for i, path in enumerate(sorted_graphs[1:], start=1):
         if verbose:
             print(f"TEST {i} ({path})")
         path = workflow.joinpath(path)
-        
-     
         wf_real = pickle.loads(path.joinpath("base_graph.pickle").read_bytes())
-
         results.setdefault(wf_real.name, {})
 
         for j, base in enumerate(sorted_graphs[:i+1]):           
@@ -82,13 +85,16 @@ def main():
                 print(f"No Microstructures Error")
                 continue
 
-            dist = compare(wf_synth, wf_real)
-            results[wf_real.name][wf_synth.name] = {
-                "real": wf_real.order(),
-                "synth": wf_synth.order(),
-                "base": summary['base_graphs'][base]['order'],
-                "dist": dist
-            }
+            dist = results.get(wf_real.name, {}).get(wf_synth.name, {}).get("dist")
+            not_in_cache = dist is None
+            if not_in_cache:
+                dist = compare(wf_synth, wf_real)
+                results[wf_real.name][wf_synth.name] = {
+                    "real": wf_real.order(),
+                    "synth": wf_synth.order(),
+                    "base": summary['base_graphs'][base]['order'],
+                    "dist": dist
+                }
 
             rows[j][i] = dist
             if verbose:
@@ -96,11 +102,12 @@ def main():
                 print(dist)
                 print()
 
-        workflow.joinpath("results").with_suffix(".json").write_text(json.dumps(results, indent=2)) 
-        df = pd.DataFrame(rows, columns=labels, index=labels)
-        df = df.dropna(axis=1, how='all')
-        df = df.dropna(axis=0, how='all')
-        workflow.joinpath("results.csv").write_text(df.to_csv())
+            if not_in_cache: # if it's cached, no need to write
+                results_path.write_text(json.dumps(results, indent=2)) 
+                df = pd.DataFrame(rows, columns=labels, index=labels)
+                df = df.dropna(axis=1, how='all')
+                df = df.dropna(axis=0, how='all')
+                workflow.joinpath("results.csv").write_text(df.to_csv())
     
 if __name__ == "__main__":
     main()
